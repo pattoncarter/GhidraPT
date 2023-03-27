e1934d93842e389f531a0330e03383909b7fb7e4
Daxton Gilliam <daxton@client-10-229-154-18.tamulink.tamu.edu>
2023-03-27 08:57:41 -0500

    simplified code

--- GhidraPT.py
+++ GhidraPT.py
@@ -10,27 +10,7 @@ import logging
 import httplib
 
 # Set your API key
-api_key = 'API KEY'
-
-
-def send_https_request(address, path, data, headers):
-    try:
-        conn = httplib.HTTPSConnection(address)
-        json_req_data = json.dumps(data)
-        conn.request("POST", path, json_req_data, headers)
-        response = conn.getresponse()
-        json_data = response.read()
-        conn.close()
-        try:
-            data = json.loads(json_data)
-            return data
-        except ValueError:
-            logging.error("Could not parse JSON response from OpenAI!")
-            logging.debug(json_data)
-            return None
-    except Exception as e:
-        logging.error("Error sending HTTPS request: {e}".format(e=e))
-        return None
+api_key = 'API-Key'
 
 
 # Function to get the highlighted text from the decompile panel in Ghidra
@@ -46,41 +26,43 @@ def get_highlighted_text():
 recompiled_code = get_highlighted_text()
 if recompiled_code:
     PROMPT = (
-        "Please analyze the following recompiled code from Ghidra and provide a "
-        "summary of its functionality. Include information about the main "
-        "functions and important variables."
-	"Rewrite the code, changing the variable names to have more meaning,"
-	"also apply standard coding procedures."
-        "\n\nRecompiled code:\n\n" + str(recompiled_code) + "\n\nAnalysis:"
+            "Please analyze the following recompiled code from Ghidra and provide a "
+            "summary of its functionality. Include information about the main "
+            "functions and important variables."
+            "Rewrite the code, changing the variable names to have more meaning,"
+            "also apply standard coding procedures."
+            "\n\nRecompiled code:\n\n" + str(recompiled_code) + "\n\nAnalysis:"
     )
 
 ENGINE = "text-davinci-003"
 
-MAX_TOKENS = 512
+MAX_TOKENS = 2048
 
 
-def openai_request(prompt=PROMPT, temperature=0.19, max_tokens=MAX_TOKENS, engine=ENGINE):
+def openai_query(prompt=PROMPT, temperature=0.19, max_tokens=MAX_TOKENS, engine=ENGINE):
     data = {
         "model": engine,
         "prompt": prompt,
         "max_tokens": max_tokens,
         "temperature": temperature
     }
-    # The URL is "https://api.openai.com/v1/completions"
+    # The open ai api link is "https://api.openai.com/v1/completions"
     host = "api.openai.com"
     path = "/v1/completions"
-    headers = {
+    key_file = {
         "Content-Type": "application/json",
         "Authorization": "Bearer {openai_api_key}".format(openai_api_key=api_key)
     }
-    data = send_https_request(host, path, data, headers)
-    if data is None:
-        logging.error("OpenAI request failed!")
-        return None
+    httpconn = httplib.HTTPSConnection(host)
+    json_req_data = json.dumps(data)
+    httpconn.request("POST", path, json_req_data, key_file)
+    httpconn_response = httpconn.getresponse()
+    json_data = httpconn_response.read()
+    httpconn.close()
+    data = json.loads(json_data)
     logging.info("OpenAI request succeeded!")
     logging.info("Response: {data}".format(data=data))
     return data
-
 
 
 def format_response(response, max_width=80):
@@ -97,6 +79,7 @@ def format_response(response, max_width=80):
 
     return "\n" + formatted_response.strip() + "\n"
 
-response = openai_request()
+
+response = openai_query()
 result_text = response['choices'][0]['text']
 print(result_text)
