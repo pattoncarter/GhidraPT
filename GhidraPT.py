48e50484c51ac31c4d9cc918602c60611081e059
ansthomp <ansthomp@gmail.com>
2023-04-11 17:09:18 -0500

    dax's code and basic file sys

--- GhidraPT.py
+++ GhidraPT.py
@@ -1,3 +1,7 @@
+# @Author: Carter Patton and Daxton Gilliam
+# @Functionality: This script utilizes chatGPT to analyze the highlighted recompiled code given by Ghidra.
+
+import json
 import re
 import textwrap
 from ghidra.app.decompiler import DecompInterface
@@ -10,28 +14,7 @@ import os
 from datetime import datetime
 
 # Set your API key
-
-api_key = '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'
-
-
-def send_https_request(address, path, data, headers):
-    try:
-        conn = httplib.HTTPSConnection(address)
-        json_req_data = json.dumps(data)
-        conn.request("POST", path, json_req_data, headers)
-        response = conn.getresponse()
-        json_data = response.read()
-        conn.close()
-        try:
-            data = json.loads(json_data)
-            return data
-        except ValueError:
-            logging.error("Could not parse JSON response from OpenAI!")
-            logging.debug(json_data)
-            return None
-    except Exception as e:
-        logging.error("Error sending HTTPS request: {e}".format(e=e))
-        return None
+api_key = 
 
 
 # Function to get the highlighted text from the decompile panel in Ghidra
@@ -47,45 +30,49 @@ def get_highlighted_text():
 recompiled_code = get_highlighted_text()
 if recompiled_code:
     PROMPT = (
-        "Please analyze the following recompiled code from Ghidra and provide a "
-        "summary of its functionality. Include information about the main "
-        "functions and important variables."
-	"Rewrite the code, changing the variable names to have more meaning,"
-	"also apply standard coding procedures."
-        "\n\nRecompiled code:\n\n" + str(recompiled_code) + "\n\nAnalysis:"
+            "Please analyze the following recompiled code from Ghidra and provide a "
+            "summary of its functionality. Include information about the main "
+            "functions and important variables. Give the name of the function in at the start in the format"
+	    "'function=FUNCTION_NAME'"
+            "Rewrite the code, changing the variable names to have more meaning,"
+            "also apply standard coding procedures, format the code to look good."
+            "\n\nRecompiled code:\n\n" + str(recompiled_code) + "\n\nAnalysis:"
     )
 
 ENGINE = "text-davinci-003"
 
-MAX_TOKENS = 512
+MAX_TOKENS = 2048
 
 
-def openai_request(prompt=PROMPT, temperature=0.19, max_tokens=MAX_TOKENS, engine=ENGINE):
+def openai_query(prompt=PROMPT, temperature=0.19, max_tokens=MAX_TOKENS, engine=ENGINE):
     data = {
         "model": engine,
         "prompt": prompt,
         "max_tokens": max_tokens,
         "temperature": temperature
     }
-    # The URL is "https://api.openai.com/v1/completions"
-    host = "api.openai.com"
-    path = "/v1/completions"
+    endpoint_url = "api.openai.com/v1/completions"
     headers = {
         "Content-Type": "application/json",
         "Authorization": "Bearer {openai_api_key}".format(openai_api_key=api_key)
     }
-    data = send_https_request(host, path, data, headers)
-    if data is None:
-        logging.error("OpenAI request failed!")
-        return None
+    connection = httplib.HTTPSConnection("api.openai.com")
+    json_req_data = json.dumps(data)
+    connection.request("POST", "/v1/completions", json_req_data, headers)
+    connection_response = connection.getresponse()
+    response_data = connection_response.read()
+    connection.close()
+    parsed_data = json.loads(response_data)
     logging.info("OpenAI request succeeded!")
-    logging.info("Response: {data}".format(data=data))
-    return data
+    logging.info("Response: {data}".format(data=parsed_data))
+    return parsed_data
 
 
-def format_response(response, max_width=80):
+def format_response(api_response, max_width=80):
+    # Pull function name
+    functino
     # Split the response into sentences
-    sentences = re.split('(?<=[.!?]) +', response)
+    sentences = re.split('(?<=[.!?]) +', api_response)
 
     # Initialize the formatted response string
     formatted_response = ""
@@ -97,16 +84,27 @@ def format_response(response, max_width=80):
 
     return "\n" + formatted_response.strip() + "\n"
 
-response = openai_request()
+
+response = openai_query()
 result_text = response['choices'][0]['text']
+print("Prompt:\n\n"+PROMPT)
 print(result_text)
 
 # append results to the end of a file for future use
 #get file path for file containing the executable - results are stored in the same folder
 cwd = currentProgram.getExecutablePath()
 name_size = len(currentProgram.getName())
-filename = cwd[:-(name_size)] + "results.txt"
-with open(filename, 'a+') as file:
+filepath = cwd[:-(name_size)]
+with open(filepath + "results.txt", 'a+') as file:
     file.write("Run at " + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n")
     file.write(result_text)
     file.write("\n\n---------------------------\n\n")
+
+# append all functions together - running the script multiple times on the same function will add that function multiple times to the file
+# function = re.search("Rewritten code:\\n\\n+(.*)$", result_text).group(1)
+# print(function)
+# with open(filepath + "functions.c", "a+") as file:
+#     file.write(function)
+
+
+# sort by function name, or have one file that compiles all the rewritten functions together?
