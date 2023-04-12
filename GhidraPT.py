e21c6105a0569ae49bfbff495331cc4c85acd4d9
ansthomp <ansthomp@gmail.com>
2023-04-11 20:40:12 -0500

    get vars, get calling/called fncs methods

--- GhidraPT.py
+++ GhidraPT.py
@@ -13,7 +13,67 @@ import os
 from datetime import datetime
 
 # Set your API key
-api_key = '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'
+api_key = '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'
+
+# setting up writing to file
+cwd = currentProgram.getExecutablePath()
+name_size = len(currentProgram.getName())
+filepath = cwd[:-(name_size)]
+fm = currentProgram.getFunctionManager()
+# TODO: make file for all folders to go into, rn errno 22 for inaccess, at
+# to run right now, make GhidraPT folder in project folder
+# os.mkdir(filepath + "GhidraPT")
+
+#### INFORMATION GATHERING HELPER FUNCTIONS ####
+
+# adds all function names for program to a text file
+# TODO: filter out thunk functions
+def get_function_names():
+    function = getFirstFunction()
+    with open(filepath + "GhidraPT/functions.txt", 'w') as file:
+        while function is not None:
+            file.write(function.getName() + '\n')
+            function = getFunctionAfter(function)
+
+# returns all functions the current function (at the current address) calls
+# currently rewrites file every time it's run, so it's specific to the function in the file
+def get_called_functions():
+    fnc = fm.getFunctionContaining(currentAddress)
+    with open(filepath + "GhidraPT/called_functions.txt", 'w') as file:
+        all_fncs = fnc.getCalledFunctions(monitor)
+        for f in all_fncs:
+            try:
+                pfnc = re.search("::(.*)", f.toString()).group(1)
+                file.write(pfnc + '\n')
+            except Exception as e:
+                file.write(f.toString())
+
+
+
+# returns all functions that call the current function (at the current address)
+# currently rewrites file every time it's run, so it's specific to the function in the file
+def get_calling_functions():
+    fnc = fm.getFunctionContaining(currentAddress)
+    with open(filepath + "GhidraPT/calling_functions.txt", 'w') as file:
+        all_fncs = fnc.getCallingFunctions(monitor)
+        for f in all_fncs:
+            file.write(f.toString() + '\n')
+
+# return all variables in the current function
+# NOTE: gets the local variable names, not meaningful names
+# tried to get from decompile, however it outs in a string, so there's not a parser for fncs in a string
+def get_variables_in_function():
+    # currentProgram = getCurrentProgram()
+    # decompiler = DecompInterface()
+    # decompiler.openProgram(currentProgram)
+    fnc = getFunctionContaining(currentAddress)
+    # decfnc = decompiler.decompileFunction(fnc, 5, monitor).getDecompiledFunction().getC()
+    # fnc = currentLocation.getDecompile().getFunction()
+    with open(filepath + "GhidraPT/variables.txt", 'w') as file:
+        all_vars = fnc.getVariables(None)
+        for v in all_vars:
+            file.write(v.getName() + '\n')
+
 
 
 # Function to get the highlighted text from the decompile panel in Ghidra
@@ -23,6 +83,11 @@ def get_highlighted_text():
     decompiler.openProgram(currentProgram)
 
     highlighted_text = currentLocation.getDecompile().getCCodeMarkup()
+    # highlight1 = state.getCurrentHighlight().toString()
+    # print("code decompile")
+    # # print(decompiler.decompileFunction(currentLocation, 5, monitor))
+    # print("highlighted text")
+    # print(highlight1)
     return highlighted_text
 
 
@@ -84,19 +149,24 @@ def format_response(api_response, max_width=80):
     return "\n" + formatted_response.strip() + "\n"
 
 
-response = openai_query()
-result_text = response['choices'][0]['text']
-print("Prompt:\n\n"+PROMPT)
-print(result_text)
+# response = openai_query()
+# result_text = response['choices'][0]['text']
+# print("Prompt:\n\n"+PROMPT)
+# print(result_text)
 
 # append results to the end of a file for future use
 #get file path for file containing the executable - results are stored in the same folder
-cwd = currentProgram.getExecutablePath()
-name_size = len(currentProgram.getName())
-filepath = cwd[:-(name_size)]
-with open(filepath + "results.txt", 'a+') as file:
+# cwd = currentProgram.getExecutablePath()
+# name_size = len(currentProgram.getName())
+# filepath = cwd[:-(name_size)]
+get_function_names()
+get_called_functions()
+get_calling_functions()
+get_variables_in_function()
+# get_highlighted_text()
+with open(filepath + "GhidraPT/results.txt", 'a+') as file:
     file.write("Run at " + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n")
-    file.write(result_text)
+    # file.write(result_text)
     file.write("\n\n---------------------------\n\n")
 
 # append all functions together - running the script multiple times on the same function will add that function multiple times to the file
